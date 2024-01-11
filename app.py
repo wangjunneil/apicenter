23ae81768eb9fbc6f7cf956d4b5c974badf53cc9
哲剑 <zhejian.wj@alipay.com>
2024-01-11 17:31:50 +0800

    refactor code

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from flask_restful import Api
 from dotenv import load_dotenv, find_dotenv
 
 from tools.auth import auth
-from modules.chat_gpt import GptAnswer
+from modules.chat_gpt import GPTAnswer
 
 # 初始化
 app = Flask(__name__)
 api = Api(app)
 
-# Restful路由
-api.add_resource(GptAnswer, '/v1/gpt')
-
+# Restful 路由
+api.add_resource(GPTAnswer, '/v1/gpt')
 
 @app.route('/')
 def status():
@@ -20,16 +19,17 @@ def status():
     """    
     return {'message':'The API Server is Running.'}, 200
 
-
 @app.before_request
 @auth.login_required
 def before_request():
-    # 每个请求处理之前执行
-    # 在这个函数中不需要做任何事情，因为认证逻辑在 auth.verify_password 中处理
+    """
+    拦截所有业务请求，本函数不做处理，递交到 auth.verify_password 中处理
+    """
     pass
 
 if __name__ == '__main__':
-    # 加载配置
+    
+    # .env 配置加载
     load_dotenv(find_dotenv(), override=True)
     
     app.run(debug=False, host='0.0.0.0', port=5678)
--- modules/chat_gpt.py
+++ modules/chat_gpt.py
@@ -1,30 +1,36 @@
 from flask_restful import Resource, request
 from openai import OpenAI
 import httpx
-
-
 from tools.kms import get_secret
 
-class GptAnswer(Resource):
+class GPTAnswer(Resource):
+    """ GPT 服务类 """
     
     def __init__(self) -> None:
+        """ 默认构造 """
         super().__init__()
         
+        # 读取 kms 中 openai 的配置信息
         openai_api_key = get_secret('OPENAI_API_KEY')
         openai_proxy_url = get_secret('OPENAI_PROXY_URL')
         
+        # 初始化 openai client
         self.client = OpenAI(
             api_key=openai_api_key,
             http_client=httpx.Client(proxy=openai_proxy_url)
         )
         
     def get(self):
+        """ GET 请求 """
         request_data = request.get_json()
         content = request_data['content']
-        result = self.__chat(message=content)
+        result = self.__chat([
+            { "role":"user", "content":content }
+        ])
         return result, 200
         
     def __chat(self, message):
+        """ 私有调用GPT服务方法 """
         chat_completion = self.client.completions.create(
             message=message,
             model="gpt-3.5-turbo",
